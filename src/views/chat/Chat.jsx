0c7d6c9ac6cc2962045e8587450590e43592f438
Nenad Drobac <nenad.d@enginee.rs>
2021-04-27 18:56:06 +0200

    Initial setup and design

---
+++ src/views/chat/Chat.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./style.scss";
+
+const Chat = () => {
+  React.useEffect(() => {});
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <div className="message-wrapper">
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57martin57martin57 martin57martin57martin57martin57martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+          <div className="message-buble">
+            <div className="sender">martin57</div>
+            <div className="message">martin57</div>
+            <div className="timestamp">10 Mar 2018 10:19</div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-wrapper">
+        <div className="input-wrapper">
+          <input placeholder="Message"></input>
+          <button>Send</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chat;
